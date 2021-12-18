4259c78b481630c977d43fefa625fe98a9841f4d
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-18 08:36:33 +0300

    Scripted auto-commit on change (2021-12-18 08:36:33) by gitwatch.sh

---
+++ src/main/java/ReduceSideJoinFlightsWithAirportApp.java
@@ -0,0 +1,24 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ReduceSideJoinFlightsWithAirportApp {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: ReduceSideJoinFlightsAirportApp <inputFlights path> <inputAirport path> <output path> ");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(ReduceSideJoinFlightsWithAirportApp.class);
+        job.setJobName("Reduce side join flights with airports");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportJoinMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(AirportIDPartitioner.class);
+        job.setGroupingComparatorClass()
+    }
+}
