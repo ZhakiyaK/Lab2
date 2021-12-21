1957436249b5354253c8bb7f6f098f73766f8461
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 05:45:43 +0300

    Scripted auto-commit on change (2021-12-21 05:45:43) by gitwatch.sh

--- src/main/java/ReducerJoin.java
+++ src/main/java/ReducerJoin.java
@@ -8,7 +8,7 @@ import java.util.Iterator;
 public class ReducerJoin extends Reducer<AirportIDGroupingComparator, Text, Text, Text> {
     private static final String FLOAT_NUMBER_REG_EX = "^\\d+\\.\\d+$";
 
-    @Override
+
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         final Text airportName;
         Iterator<Text> valuesIterator = values.iterator();
