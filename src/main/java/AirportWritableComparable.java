cc1575741af7ef25c7e4fe24ccf382874a0a62d5
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 04:41:15 +0300

    Scripted auto-commit on change (2021-12-21 04:41:15) by gitwatch.sh

--- src/main/java/AirportWritableComparable.java
+++ src/main/java/AirportWritableComparable.java
@@ -1,2 +1,54 @@
-public class AirportWritableComparable {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritableComparable implements WritableComparable<AirportWritableComparable>  {
+    private final IntWritable airportID;
+    private final IntWritable datasetIndicator;
+
+    public AirportWritableComparable() {
+        airportID = new IntWritable(0);
+        datasetIndicator = new IntWritable(0);
+    }
+
+    public AirportIDWritableComparable(IntWritable airportID, IntWritable datasetIndicator) {
+        this.airportID = airportID;
+        this.datasetIndicator = datasetIndicator;
+    }
+
+    protected IntWritable getAirportID() {
+        return this.airportID;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        airportID.write(dataOutput);
+        datasetIndicator.write(dataOutput);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        airportID.readFields(dataInput);
+        datasetIndicator.readFields(dataInput);
+    }
+
+    @Override
+    public int compareTo(AirportIDWritableComparable o) {
+        int resultOfCompareAirportID = this.getAirportID()
+                .compareTo(
+                        o.getAirportID()
+                );
+        if (resultOfCompareAirportID == 0) {
+            return this.getDatasetIndicator()
+                    .compareTo(
+                            o.getDatasetIndicator()
+                    );
+        } else {
+            return  resultOfCompareAirportID;
+        }
+    }
 }
+
