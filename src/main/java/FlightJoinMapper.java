8709038ade0be6acd14644c363ffe0315f7a50ed
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 04:44:31 +0300

    Scripted auto-commit on change (2021-12-21 04:44:31) by gitwatch.sh

--- src/main/java/FlightJoinMapper.java
+++ src/main/java/FlightJoinMapper.java
@@ -2,5 +2,5 @@ import jdk.internal.module.ModuleLoaderMap;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
-public class FlightJoinMapper extends ModuleLoaderMap.Mapper <LongWritable, Text, AirportWritableComparable> {
+public class FlightJoinMapper extends ModuleLoaderMap.Mapper <LongWritable, Text, AirportWritableComparable, Text> {
 }
