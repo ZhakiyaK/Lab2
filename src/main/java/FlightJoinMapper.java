96df75b1d24aceb862b67aff3f9053501c64684d
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 04:56:07 +0300

    Scripted auto-commit on change (2021-12-21 04:56:07) by gitwatch.sh

--- src/main/java/FlightJoinMapper.java
+++ src/main/java/FlightJoinMapper.java
@@ -1,7 +1,36 @@
 import jdk.internal.module.ModuleLoaderMap;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import javax.print.attribute.standard.Destination;
+import java.io.IOException;
 
 public class FlightJoinMapper extends ModuleLoaderMap.Mapper <LongWritable, Text, AirportWritableComparable, Text> {
-    private class final 
+    private static final String SEPERATOR = ",";
+    private static final String CSV_COLOUM_NAME = "\"DEST_AIRPORT_ID\"";
+    private static final int Destination_AIRPORT_ID_INDEX = 14;
+    private static final int DELAY_INDEX = 17;
+
+
+
+
+
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException, NumberFormatException {
+        String[] values = value
+                .toString()
+                .split(SEPERATOR);
+        String airportIDString = value[Destination_AIRPORT_ID_INDEX];
+
+        if (!airportIDString.equals((CSV_COLOUM_NAME)) {
+            int airportID = Integer.parseInt(airportIDString);
+            String delay = value[DELAY_INDEX];
+            if (delay.length() != 0) {
+                context.write();
+            }
+
+        }
+    }
 }
