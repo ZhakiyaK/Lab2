024c1166a7a057ac74c6bd01935ab7e41c0d67b6
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 05:45:19 +0300

    Scripted auto-commit on change (2021-12-21 05:45:19) by gitwatch.sh

--- src/main/java/ReducerJoin.java
+++ src/main/java/ReducerJoin.java
@@ -1,9 +1,57 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ReducerJoin extends Reducer<AirportIDGroupingComparator, Text, Text, Text> {
     private static final String FLOAT_NUMBER_REG_EX = "^\\d+\\.\\d+$";
 
     @Override
-    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws 
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        final Text airportName;
+        Iterator<Text> valuesIterator = values.iterator();
+        airportName = new Text(
+                valuesIterator
+                        .next()
+                        .toString()
+        );
+        ArrayList<String> delays = getDelays(valuesIterator);
+        if (delays.size() > 0) {
+            context.write(
+                    airportName,
+                    computeMinMaxAvarageDelay(delays)
+            );
+
+        }
+    }
+    protected ArrayList<String> getDelays(Iterator<Text> valuesIteraror) {
+        ArrayList<String> delays = new ArrayList<>();
+        while(valuesIteraror.hasNext()) {
+            String delay = valuesIteraror.next().toString();
+            if (delay.matches(FLOAT_NUMBER_REG_EX)) {
+                delays.add(delay);
+            }
+        }
+        return delays;
+    }
+    protected Text computeMinMaxAvarageDelay(ArrayList<String> delays) {
+        float min = Float.MAX_VALUE, max = 0, sum = 0;
+        for (String delay: delays) {
+            float delayFloatValue = Float.parseFloat(delay);
+            if (delayFloatValue < min) {
+                min = delayFloatValue;
+            }
+            if (delayFloatValue > max) {
+                max = delayFloatValue;
+            }
+            sum += delayFloatValue;
+        }
+        return new Text(
+                "min = " + min
+                + ", avarage = " + sum/delays.size()
+                + ", max = " +max
+        );
+    }
 }
