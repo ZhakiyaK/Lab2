2281c02f1703f3ee28066462c8193b43d057a436
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 05:30:28 +0300

    Scripted auto-commit on change (2021-12-21 05:30:28) by gitwatch.sh

--- src/main/java/ReducerJoin.java
+++ src/main/java/ReducerJoin.java
@@ -1,2 +1,9 @@
-public class ReduceJoin {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ReducerJoin extends Reducer<AirportIDGroupingComparator, Text, Text, Text> {
+    private static final String FLOAT_NUMBER_REG_EX = "^\\d+\\.\\d+$";
+
+    @Override
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws 
 }
