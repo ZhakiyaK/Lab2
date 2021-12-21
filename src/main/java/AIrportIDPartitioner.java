91da1e441d04a29271cd1f686365d50eb8adbe20
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 05:18:28 +0300

    Scripted auto-commit on change (2021-12-21 05:18:28) by gitwatch.sh

--- src/main/java/AIrportIDPartitioner.java
+++ src/main/java/AIrportIDPartitioner.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class AIrportIDPartitioner extends Partitioner <AirportWritableComparable, Text> {
+public class AirportIDPartitioner extends Partitioner <AirportWritableComparable, Text> {
 
     @Override
     public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int numReduceTasks) {
