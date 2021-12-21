9068f05905100157c5d625df7d30e4a1ebd5b71d
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-21 05:25:02 +0300

    Scripted auto-commit on change (2021-12-21 05:25:02) by gitwatch.sh

--- src/main/java/AirportIDGroupingComparator.java
+++ src/main/java/AirportIDGroupingComparator.java
@@ -1,2 +1,20 @@
-public class AirportIDGroupingComparator {
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableComparable;
+
+public class AirportIDGroupingComparator extends WritableComparator {
+
+    public AirportIDGroupingComparator() {
+        super(AirportWritableComparable.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        AirportWritableComparable id1 = (AirportWritableComparable) a;
+        AirportWritableComparable id2 = (AirportWritableComparable) b;
+        return id1
+                .getAirportID()
+                .compareTo(id2
+                .getAirportID()
+                );
+    }
 }
