a3573ae2ed7203f4dbac31b15b22e2e9ff08d5b5
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-25 01:15:18 +0300

    Scripted auto-commit on change (2021-12-25 01:15:17) by gitwatch.sh

--- src/main/java/AirportAnalyzerApp.java
+++ src/main/java/AirportAnalyzerApp.java
@@ -1,2 +1,40 @@
+import org.apache.spark.SparkConf
+
+import java.util.Map;
+
 public class AirportAnalyzerApp {
+    private static final String SPARK_APP_NAME = "Airport analyzer";
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf ().setAppName(SPARK_APP_NAME);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaPairRDD<Tuple2<String, String>, FlightDelay> flightsDelays = parseFlightDelaysFromCSV(sc);
+
+        JavaPairRDD<Tuple2<String, String>, DelayStat> delaysStat = flightsDelays.combineByKey(
+                DelaysStat::new;
+                DelaysStat::addDelay;
+                DelayStat::add
+        );
+
+        JavaPairRDD<String, String> airportNames = parseAirportFromCSV(sc);
+
+        final Broadcast<Map<String, String>> airportBroadcast = sc.broadcast(airportNames.collectAsMap());
+
+        JavaRDD<DelaysStatWithAirportNames> parsedData = delaysStat.map(
+                delaysBtwAirport -> new DelaysStatWithAirportNames(
+                        delaysBtwAirport._1(),
+                        delaysBtwAirport._2(),
+                        airportBroadcast.value()
+                )
+        );
+
+        parsedData.saveAsTextFile
+    }
 }
