28ac1806815e6400c187d4419edf4e8b56ff7cc9
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-25 01:35:05 +0300

    Scripted auto-commit on change (2021-12-25 01:35:05) by gitwatch.sh

--- src/main/java/DelaysStat.java
+++ src/main/java/DelaysStat.java
@@ -1,2 +1,22 @@
-public class DelaysStat {
+import java.io.Serializable;
+
+public class DelaysStat implements Serializable {
+
+    private static final int
+
+    private float   delayedCount;
+    private float   cancelledCount;
+    private float   maxDelay;
+    private float   flightsCount;
+
+    protected DelaysStat(float maxDelay, int flightsCounts, float delayedCount, float cancelledCount) {
+        this.maxDelay = maxDelay;
+        this.flightsCount = flightsCounts;
+        this.delayedCount = delayedCount;
+        this.cancelledCount = cancelledCount;
+    }
+
+    private void updateDelaysStat(FlightDelay flightDelay) {
+        if (flightDelay.getCancelledStatus()) {
+    }
 }
