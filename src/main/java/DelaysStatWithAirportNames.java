74b6de0ad62c04e43d6f5a623c63f86f50b4a966
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-25 02:01:11 +0300

    Scripted auto-commit on change (2021-12-25 02:01:11) by gitwatch.sh

--- src/main/java/DelaysStatWithAirportNames.java
+++ src/main/java/DelaysStatWithAirportNames.java
@@ -1,2 +1,35 @@
-public class DelaysStatWithAirportNames {
+public class DelaysStatWithAirportNames extends DelaysStat {
+    private static final String FLOAT_STRING_FORMAT = "%.2f";
+    private static final int    PERCENT_CONVERSION_CONSTANT = 100;
+
+    private final String        departureAirportName;
+    private final String        destinationAirportName;
+
+
+
+    public DelaysStatWithAirportNames(Tuple2<String, String> airportNames,DelaysStat delaysStatSrc, Map<String, String> airportName) {
+        super (
+                delaysStatSrc.getMaxDelay(),
+                delaysStatSrc.getFlightsCount(),
+                delaysStatSrc.getDelayedCount(),
+                delaysStatSrc.getCancelledCount(),
+        );
+
+        this.departureAirportName = airportName.get(airportName._1());
+        this.destinationAirportName = airportName.get(airportName._2());
+    }
+
+    @Override
+    public String toString() {
+        String percentOfCancelledFlights = percentage(getCancelledCount(), getFlightsCount());
+        String percentOfDelayedFlights = percentage(getDelayedCount(), getFlightsCount());
+        return departureAirportName + " -> " + destinationAirportName +
+                "\nMax delay: " + getMaxDelay() +
+                "\n" + percentOfCancelledFlights + "% flights were cancelled" +
+                "\n" + percentOfDelayedFlights + "% flights were delayed\n";
+    }
+
+    private static String percentage(float numerator, int denominator) {
+        return String.format(FLOAT_STRING_FORMAT, numerator / denominator * PERCENT_CONVERSION_CONSTANT);
+    }
 }
