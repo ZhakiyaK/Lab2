bb78d5ecf94d673716ea51c6dd99b5d5846c4a11
ZhakiyaK <akzhol.zhakiya@gmail.com>
2021-12-25 02:41:21 +0300

    Scripted auto-commit on change (2021-12-25 02:41:21) by gitwatch.sh

--- src/main/java/AirportAnalyzerApp.java
+++ src/main/java/AirportAnalyzerApp.java
@@ -1,15 +1,22 @@
 
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 import java.util.Map;
 
 public class AirportAnalyzerApp {
     private static final String SPARK_APP_NAME = "Airport analyzer";
-    private static final String OUTPUT_FILENAME = "delays";
     private static final String HDFS_PATH_TO_FLIGHTS = "airport.csv";
+    private static final String HDFS_PATH_TO_AIRPORTS = "airports.csv";
     private static final String FLIGHTS_FILE_FIRST_LINE_PREFIX = "\"";
+    private static final String AIRPORTS_FILE_FIRST_LINE_PREFIX = "C";
     private static final String DATA_SEPERATOR = ",";
+    private static final String OUTPUT_FILENAME = "delays";
     private static final int    AIRPORT_ID_INDEX = 0;
     private static final int    AIRPORT_NAME_INDEX = 1;
 
